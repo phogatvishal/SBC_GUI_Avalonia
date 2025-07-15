2b640b72ac557a6344601055451aa71bc1663a52
shassoni <shaswat.soni@non.keysight.com>
2025-07-16 04:54:44 +0530

    test commit in - feature-linux

--- SBC.WPF/Models/LogLine.cs
+++ SBC.WPF/Models/LogLine.cs
@@ -4,6 +4,7 @@ namespace SBC.WPF.Models
 {
 	public class LogLine 
 	{
+		//test commit
 		public string? Message { get; set; } = string.Empty;
 		public IBrush Color { get; set; } = Brushes.White;
 	}
